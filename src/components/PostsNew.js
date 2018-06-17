9894f3f02b88e0c97a8c21d262d827661947b6e1
daria marczak <marczak.ria@gmail.com>
2018-06-17 21:04:39 +0200

    Added redux-form

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,13 +1,67 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderField(field) { // field contains some event handlers
+    return (
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        /> {/* Object that contains those handlers */}
+        
+      </div>
+    )
+  };
+
   render() {
     return (
-      <div>
-        PostsNew/
-      </div>
+      <form>
+        <Field 
+          label="Title"
+          name="title"
+          component={this.renderField} // Add in a function that will return some JSX used to show this field component on the screen
+        />
+        <Field 
+          label="Categories"        
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Posts content"        
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
+};
+
+function validate() {
+  const errors = {};
+  // Validate the inputs from "values" object
+  // If errors is empty, the form is fine to submit
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  };
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  };
+
+  if (!values.content) {
+    errors.title = "Please enter some content";
+  };
+
+  return errors;
 }
 
-export default PostsNew;
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
+
+// reduxForm is kind of like connect in terms of syntax
+// It is actually making a form that then transfers the data to the state
